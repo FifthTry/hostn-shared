159c1a8a3ec810b441fbab1301ae1259d1f4d606
AbrarNitk <abrar.nitk@gmail.com>
2023-02-02 19:50:12 +0530

    heroku config file changes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 ./install.sh
 
-./fastn serve --port $PORT --bind 0.0.0.0 --download-base-url $DOWNLOAD_BASE_URL
+./fastn_linux_musl_x86_64 serve --port $PORT --bind 0.0.0.0 --download-base-url $DOWNLOAD_BASE_URL
